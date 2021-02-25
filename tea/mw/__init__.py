1577fd530db1fce98f3073794979b0280254d314
陈子昂 <chenziang@antiy.cn>
2021-02-25 16:03:49 +0800

    feat: init

--- tea/mw/__init__.py
+++ tea/mw/__init__.py
@@ -1,7 +1,15 @@
 # coding: utf-8
-from contextlib import contextmanager
+import loggus
+import inspect
 
 from tea.ctx import Ctx
+from contextlib import contextmanager
+
+
+def CheckMw(*mws):
+    for mw in mws:
+        if not inspect.isgeneratorfunction(mw):
+            loggus.panic(f"mw[{mw}] is not a generator func")
 
 
 def Mw(ctx: Ctx, handler, *mw):
