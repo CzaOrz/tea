ce4bfb9dbeb1c9b3721c5402a0c8c9702c136532
陈子昂 <chenziang@antiy.cn>
2021-02-25 13:11:30 +0800

    feat: init

--- tea/wsgi/app/__init__.py
+++ tea/wsgi/app/__init__.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 
 def Application(environ, start_response):
-    start_response("200 ok")
+    start_response("200 ok", [])
+    from pprint import pprint
+    pprint(environ)
     return [b"test"]
